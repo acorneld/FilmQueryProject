8df535f6e25e7c9cc22b019b2d5e48919ac82379
Acorneld <acorneld@gmail.com>
2022-04-02 11:07:17 -0600

    Some stubbed app

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -8,37 +8,54 @@ import com.skilldistillery.filmquery.database.DatabaseAccessorObject;
 import com.skilldistillery.filmquery.entities.Actor;
 import com.skilldistillery.filmquery.entities.Film;
 import com.skilldistillery.filmquery.entities.*;
+
 public class FilmQueryApp {
-  
-  DatabaseAccessor db = new DatabaseAccessorObject();
 
-  public static void main(String[] args) {
-    FilmQueryApp app = new FilmQueryApp();
-    app.test();
+	DatabaseAccessor db = new DatabaseAccessorObject();
+
+	public static void main(String[] args) {
+		FilmQueryApp app = new FilmQueryApp();
+
+//    app.menu();
+//    app.test();
 //    app.launch();
-  }
+	}
+//BEGIN METHODS ********
+	private void test() {
+		Film film = db.findFilmById(1);
+		Actor actor = db.findActorById(1);
+		List<Actor> actorsInFilm = db.findActorsByFilmId(2);
+		System.out.println(film);
+		System.out.println("");
+		System.out.println(actor);
+		System.out.println(actorsInFilm);
+		System.out.println(film.getActorsInFilm());
+	}
 
-  private void test() {
-    Film film = db.findFilmById(1);
-    Actor actor = db.findActorById(1);
-    List<Actor> actorsInFilm = db.findActorsByFilmId(2);
-    System.out.println(film);
-    System.out.println("");
-    System.out.println(actor);
-    System.out.println(actorsInFilm);
-    System.out.println(film.getActorsInFilm());
-  }
+	private int launch() {
+		Scanner input = new Scanner(System.in);
+		menu();
+		int choice = 0;
+		choice = input.nextInt();
+		input.close();
+		return choice;
+	}
 
-  private void launch() {
-    Scanner input = new Scanner(System.in);
-    
-    startUserInterface(input);
-    
-    input.close();
-  }
-
-  private void startUserInterface(Scanner input) {
-    
-  }
-
+	private void menu() {
+		System.out.println("Welcome to the SD Movie Database.");
+		System.out.println("Please choose from the following options: ");
+		System.out.println("1. Look up Film by ID");
+		System.out.println("2. Look up Film by Keyword");
+		System.out.println("3. Exit the Application");
+	}
+	
+	private void menuChoice(int choice) {
+		switch(choice) {
+		case 1:
+			Scanner kb = new Scanner(System.in);
+			System.out.println("Please enter the Film's ID: ");
+			
+		}
+	}
+//END METHODS*************
 }
